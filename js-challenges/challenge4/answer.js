1dfa580417247019d0af199afb8364c6691ff06b
Chris Featley <f.chris@live.co.uk>
2018-11-02 17:23:54 +0000

    kind of done it, check out the comments I added within code

--- js-challenges/challenge4/answer.js
+++ js-challenges/challenge4/answer.js
@@ -2,7 +2,15 @@
 // write a function that returns the longest word in the input
 
 function longestWord(word){
-
+  let a = word.split(/\W/) //so I had to look online about this, they suggested RegEx
+  //yar cos I have a clue about RegEx :> cos I'm not immediately lost :@ :> but yeh
+  //worked out that \W was non-alphanumeric characters
+  let b = "1"
+  a.forEach(compareLength)
+  function compareLength(c){
+    if (c.length>b.length){b=c}
+  }
+  console.log(b)
 }
 
 // do not alter the below part
@@ -10,3 +18,6 @@ console.log(longestWord('hello!goodybe,yo')) // outputs: goodbye
 console.log(longestWord('rich-chris,andy')) // outputs:  chris
 console.log(longestWord('football!golf,tennis?cricket,polo')) // outputs: football
 console.log(longestWord('a.b,lol')) // outputs: lol
+
+//so not got the frig why the console log within the longestWord function works,
+//but then the console.log in lines 17-20 dont. ??
