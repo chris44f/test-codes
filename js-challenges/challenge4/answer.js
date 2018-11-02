2d23f3b695a560a5a6f0df6d731170976be4284a
Richard Matthews <rich@richards-air.home>
2018-11-02 18:29:25 +0000

    return multiple strings

--- js-challenges/challenge4/answer.js
+++ js-challenges/challenge4/answer.js
@@ -1,22 +1,23 @@
 // Part1 (yar coz you understand this is a mega challenge consisting of 5 parts :>)
 // write a function that returns the longest word in the input
 
+// Part2 adapt the function so that now if 2 words are longest I want 2 returned
+// or if 4 are longest I want all 4 returned etc
+// also please see TESTS section below and ensure output looks like that
+
 function longestWord(word){
-  let a = word.split(/\W/) //so I had to look online about this, they suggested RegEx
-  //yar cos I have a clue about RegEx :> cos I'm not immediately lost :@ :> but yeh
-  //worked out that \W was non-alphanumeric characters
+  let a = word.split(/\W/)
   let b = "1"
   a.forEach(compareLength)
   function compareLength(c){
     if (c.length>b.length){b=c}
   }
   return b
-} 
+}
 
+// TESTS
 // do not alter the below part
-console.log(longestWord('hello!goodybe,yo')) // outputs: goodbye
-console.log(longestWord('rich-chris,andy')) // outputs:  chris
-console.log(longestWord('football!golf,tennis?cricket,polo')) // outputs: football
-console.log(longestWord('a.b,lol')) // outputs: lol
-
-//so how come console.log(console.log(b)) don't work? lol
+console.log(longestWord('hello!a,yo')) // outputs: The longest string is hello
+console.log(longestWord('rich-cris,ando')) // outputs: The longest string is rich, cris, ando
+console.log(longestWord('football!golf,tennis?cricket,polo')) // outputs: The longest string is football
+console.log(longestWord('a.b,c?d')) // outputs: The longest string is a, b, c, d
