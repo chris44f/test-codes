e7c51bf899c9dab7ecd44c8af69ca2a71047f165
Rich Matthews <richard@paddle.com>
2018-11-02 15:56:29 +0000

    add new challenge

---
+++ js-challenges/challenge4/answer.js
@@ -0,0 +1,12 @@
+// Part1 (yar coz you understand this is a mega challenge consisting of 5 parts :>)
+// write a function that returns the longest word in the input
+
+function longestWord(word){
+
+}
+
+// do not alter the below part
+console.log(longestWord('hello!goodybe,yo')) // outputs: goodbye
+console.log(longestWord('rich-chris,andy')) // outputs:  chris
+console.log(longestWord('football!golf,tennis?cricket,polo')) // outputs: football
+console.log(longestWord('a.b,lol')) // outputs: lol
