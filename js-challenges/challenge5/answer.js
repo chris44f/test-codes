35dedf6125209fbd03324055255714a7b9d3d294
Richard Matthews <rich@richards-air.home>
2018-11-03 13:57:23 +0000

    Challenge 5

--- js-challenges/challenge5/answer.js
+++ js-challenges/challenge5/answer.js
@@ -1,17 +1,9 @@
-
+//
 function letterCounter(str){
-  let completedArray = []
-  let letterToFind
-  let stringArray = Array.from(str)
-  for (var i = 0; i < stringArray.length; i++) {
-    letterToFind = stringArray[i]
-    const len = stringArray.filter(w => w === letterToFind)
-    completedArray.push(`${len.length}${letterToFind}`)
-  }
-  const set = new Set(completedArray)
-  return Array.from(set).join('')
+
 }
 
-console.log(letterCounter('aaaeeffaa'))
-// letterCounter('abbcc')
-// letterCounter('klqqqq')
+console.log(letterCounter('aaaeeffaa')) // 5a2e2f
+console.log(letterCounter('abc')) // 1a1b1c
+console.log(letterCounter('zzaappll')) // 2z2a2p2l
+console.log(letterCounter('chris')) // 1c1h1r1i1s
