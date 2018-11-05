33d69586aaa0717419c73012cae3384a746a1001
Richard Matthews <rich@richards-air.home>
2018-11-05 20:32:08 +0000

    next part

--- js-challenges/challenge4/answer.js
+++ js-challenges/challenge4/answer.js
@@ -5,17 +5,19 @@
 // or if 4 are longest I want all 4 returned etc
 // also please see TESTS section below and ensure output looks like that
 
+// Part3 Adapt the current code so that now it passes the tests below
+
 function longestWord(word){
   let a = word.split(/\W/)
   let b = "1"
   let longWords = []
   a.forEach(function(c){
-    switch (true) {
+    switch (true) { // you should probably switch on an actual expression rather than a boolean
       case c.length>b.length:
-        b=c
+        b=c // you should do b = c
         longWords.push(c)
         break;
-      case c.length==b.length:
+      case c.length==b.length: // you should do c.length === b.length
         b=c
         longWords.push(c)
         break;
@@ -27,7 +29,7 @@ return "The longest string is " + longWords.join(", ")
 
 // TESTS
 // do not alter the below part
-console.log(longestWord('hello!a,yo')) // outputs: The longest string is hello
-console.log(longestWord('rich-cris,ando')) // outputs: The longest string is rich, cris, ando
-console.log(longestWord('football!golf,tennis?cricket,polo')) // outputs: The longest string is football
-console.log(longestWord('a.b,c?d')) // outputs: The longest string is a, b, c, d
+console.log(longestWord('hello!a,yo')) // outputs: Hello is the longest word and is 5 characters
+console.log(longestWord('rich-cris,ando')) // outputs: Rich, cris, andy are the longest words and are 4 characters
+console.log(longestWord('football!golf,tennis?cricket,polo')) // outputs: Football is the longest word and is 8 characters
+console.log(longestWord('a.b,c?d')) // outputs: A, b, c, d are the longest words and are 1 character
