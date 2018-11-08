f9c864203f96fcf05465d8ad2ec756f70a587cb9
Chris Featley <f.chris@live.co.uk>
2018-11-08 20:50:29 +0000

    doesnt work but almost there I think

--- js-challenges/challenge5/answer.js
+++ js-challenges/challenge5/answer.js
@@ -1,11 +1,11 @@
-//
-let str = "aaaeeffaa"
-let arr = str.split("")
-let b = 0
-let i = 0
-let strip = []
-
-arr.forEach(eVery)
+function letterCounter(str){
+  let arr = str.split("")
+  let b = 0
+  let i = 0
+  let strip = []
+  arr.forEach(eVery)
+  strip.forEach(duplicateRemover)
+}
 
 function eVery(value,index,array) {
   let a = str.charAt(index)
@@ -18,14 +18,26 @@ function eVery(value,index,array) {
   b = 0
 }
 
-console.log(strip)
+
+function duplicateRemover(val,indi,arry) {
+  let duplo = strip.lastIndexOf(val)
+  if (indi !== duplo) {
+    strip.splice(duplo,1)
+  }
+}
+
+function reducing(value,index,array) {
+  for (i=0; i <strip.length; i += 1){
+    if ( a === strip[i]){
+      b += 1
+    }
+  }
+  strip.push(b+value)
+  b = 0
+}
 
 
-
-//function letterCounter(str){
-
-
-//console.log(letterCounter('aaaeeffaa')) // 5a2e2f
-//console.log(letterCounter('abc')) // 1a1b1c
-//console.log(letterCounter('zzaappll')) // 2z2a2p2l
-//console.log(letterCounter('chris')) // 1c1h1r1i1s
+console.log(letterCounter('aaaeeffaa')) // 5a2e2f
+console.log(letterCounter('abc')) // 1a1b1c
+console.log(letterCounter('zzaappll')) // 2z2a2p2l
+console.log(letterCounter('chris')) // 1c1h1r1i1s
