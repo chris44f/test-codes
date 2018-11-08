a3cc705f4a8773a16a3a5b039e0c50e4e2d311b0
Chris Featley <f.chris@live.co.uk>
2018-11-08 21:14:49 +0000

    very almost there, just dont know how to loop through until all duplicates gone..

--- js-challenges/challenge5/answer.js
+++ js-challenges/challenge5/answer.js
@@ -3,39 +3,24 @@ function letterCounter(str){
   let b = 0
   let i = 0
   let strip = []
-  arr.forEach(eVery)
-  strip.forEach(duplicateRemover)
-}
-
-function eVery(value,index,array) {
-  let a = str.charAt(index)
-  for (i=0; i < str.length; i += 1){
-    if ( a === str.charAt(i)){
-      b += 1
+  arr.forEach(function(value,index,array) {
+    let a = str.charAt(index)
+    for (i=0; i < str.length; i += 1){
+      if ( a === str.charAt(i)){
+        b += 1
+      }
     }
-  }
-  strip.push(b+value)
-  b = 0
-}
-
-
-function duplicateRemover(val,indi,arry) {
-  let duplo = strip.lastIndexOf(val)
-  if (indi !== duplo) {
-    strip.splice(duplo,1)
-  }
-}
-
-function reducing(value,index,array) {
-  for (i=0; i <strip.length; i += 1){
-    if ( a === strip[i]){
-      b += 1
+    strip.push(b+value)
+    b = 0
+  })
+  strip.forEach(function(val,indi,arry) {
+    let duplo = strip.lastIndexOf(val)
+    if (indi !== duplo) {
+      strip.splice(duplo,1)
     }
-  }
-  strip.push(b+value)
-  b = 0
+  })
+return strip
 }
-
 
 console.log(letterCounter('aaaeeffaa')) // 5a2e2f
 console.log(letterCounter('abc')) // 1a1b1c
